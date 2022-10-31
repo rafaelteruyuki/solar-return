045c33083cab9b6ce208cb69537ed181329a8882
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2022-10-31 09:04:52 -0300

    Basic data

---
+++ app/src/data/signs.ts
@@ -0,0 +1,78 @@
+import { Sign } from "../types/sign.interface";
+
+const SIGNS: Sign[] = [
+  {
+    name: 'Aries',
+    element: 'fire',
+    modality: 'cardinal',
+    naturalHouse: 1
+  },
+  {
+    name: 'Taurus',
+    element: 'earth',
+    modality: 'fixed',
+    naturalHouse: 2
+  },
+  {
+    name: 'Gemini',
+    element: 'air',
+    modality: 'mutable',
+    naturalHouse: 3
+  },
+  {
+    name: 'Cancer',
+    element: 'water',
+    modality: 'cardinal',
+    naturalHouse: 4
+  },
+  {
+    name: 'Leo',
+    element: 'fire',
+    modality: 'fixed',
+    naturalHouse: 5
+  },
+  {
+    name: 'Virgo',
+    element: 'earth',
+    modality: 'mutable',
+    naturalHouse: 6
+  },
+  {
+    name: 'Libra',
+    element: 'air',
+    modality: 'cardinal',
+    naturalHouse: 7
+  },
+  {
+    name: 'Scorpio',
+    element: 'water',
+    modality: 'fixed',
+    naturalHouse: 8
+  },
+  {
+    name: 'Sagittarius',
+    element: 'fire',
+    modality: 'mutable',
+    naturalHouse: 9
+  },
+  {
+    name: 'Capricorn',
+    element: 'earth',
+    modality: 'cardinal',
+    naturalHouse: 10
+  },
+  {
+    name: 'Aquarius',
+    element: 'air',
+    modality: 'fixed',
+    naturalHouse: 11
+  },
+  {
+    name: 'Pisces',
+    element: 'water',
+    modality: 'mutable',
+    naturalHouse: 12
+  }
+]
+
+export default SIGNS;
